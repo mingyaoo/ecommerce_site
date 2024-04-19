1787561d6707fd74b4202cf4312aad5430336876
mingyaoo <mac.m@oundleschool.org.uk>
2024-04-19 09:45:05 +0100

    web

--- connection.php
+++ connection.php
@@ -1,18 +1,22 @@
-
 <?php
+// setting variables which will be used to login to databases via XAMPP
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "project";
+// database name
 
 try {
+    // passing variables through in order to loog in to database
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; 
+    // if database is succesfull logged in, success message is returned
     }
 catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
+    // vice versa, if it doesnt work then an eror message is sent
     }
 ?>
--- install.php
+++ install.php
@@ -1,5 +1,9 @@
 <?php
+// linking to connection.php to gain access to the database
 include_once("connection.php");
+
+// creating 
+// First table for users
 $stmt = $conn->prepare("DROP TABLE IF EXISTS TblUsers;
 CREATE TABLE TblUsers 
 (UserID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -13,4 +17,40 @@ PhoneNo INT(11) NOT NULL,
 Type TINYINT(1))");
 $stmt->execute();
 $stmt->closeCursor();
+
+// second table for products
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblProducts;
+CREATE TABLE TblProducts 
+(ProductID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+ProductName VARCHAR(100) NOT NULL,
+Price INT(10) NOT NULL,
+Description VARCHAR(300) NOT NULL,
+Category VARCHAR(300) NOT NULL,
+ItemImage VARCHAR(100) NOT NULL,
+Quantity INT(6))");
+$stmt->execute();
+$stmt->closeCursor();
+
+
+// third table for basket
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblBasket;
+CREATE TABLE TblBasket 
+(OrderNo INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+UserID INT(6) NOT NULL,
+Date_bought DATE NOT NULL,
+Delivery_date DATE NOT NULL,
+Delivered INT(1) NOT NULL,
+Paid INT(1) NOT NULL)");
+$stmt->execute();
+$stmt->closeCursor();
+
+// fourth table for basket contents
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblBasketContent;
+CREATE TABLE TblBasketContent 
+(OrderNo INT(6) NOT NULL,
+ProductID INT(6) NOT NULL,
+Quantity INT(2) NOT NULL)");
+$stmt->execute();
+$stmt->closeCursor();
+
 ?>
