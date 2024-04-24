336bc80fba799408681264106f529c52c531d784
mingyaoo <mac.m@oundleschool.org.uk>
2024-04-24 20:55:09 +0100

    web

--- homepage.php
+++ homepage.php
@@ -1,28 +1,15 @@
 
 <!DOCTYPE html>
 <html>
-    <?php
-    // linking to connection page to gain access to the database
-    include_once("connection.php")
-
-    // checking authorised user
-    session_start(); 
-    $_SESSION['backURL'] = $_SERVER['REQUEST_URI'];
-    if (!isset($_SESSION['name'])){   
-        header("Location:login.php");
-    }
-    // if incorrect it will redirect to the log in page
-    ?>
 
     <head>
         <title>Home Page</title>  
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
-        <!-- navigation bar -->
         <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="javascript:void(0)">Logo</a>
@@ -30,46 +17,42 @@
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="mynavbar">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0)">Link</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0)">Link</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0)">Link</a>
+                <ul class= "nav nav-pills">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">categories</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Link 1</a></li>
+                            <li><a class="dropdown-item" href="#">Link 2</a></li>
+                            <li><a class="dropdown-item" href="#">Link 3</a></li>
+                        </ul>
                     </li>
                 </ul>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                        Dropdown link
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Link 1</a>
-                        <a class="dropdown-item" href="#">Link 2</a>
-                        <a class="dropdown-item" href="#">Link 3</a>
-                    </div>
-                </li>
                 <form class="d-flex">
                     <input class="form-control me-2" type="text" placeholder="Search">
                     <button class="btn btn-primary" type="button">Search</button>
                 </form>
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0)">Link</a>
+                    <a class="nav-link" href="javascript:void(0)">favourites</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0)">Link</a>
+                    <a class="nav-link" href="javascript:void(0)">notifications</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0)">Link</a>
+                    <a class="nav-link" href="javascript:void(0)">profile</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)">cart</a>
                     </li>
                 </ul>
                 </div>
             </div>
         </nav>
 
+        <div class = "container", style="background-color: teal">
+            <h3 style="text-align: centre;"> carousel photos</h3>
+
+        </div>
     
 
 
--- login.php
+++ login.php
@@ -17,3 +17,17 @@
 
 </body>
 </html>
+
+
+<?php
+    // linking to connection page to gain access to the database
+    include_once("connection.php")
+
+    // checking authorised user
+    session_start(); 
+    $_SESSION['backURL'] = $_SERVER['REQUEST_URI'];
+    if (!isset($_SESSION['name'])){   
+        header("Location:login.php");
+    }
+    // if incorrect it will redirect to the log in page
+    ?>
